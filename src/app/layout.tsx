49cbb8bef91e0950043e7bdd3d6fc5efcb8f42df
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2025-01-03 00:05:47 +0530

    fix: update font path in layout for correct loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 const clash = localFont({
   src: [
     {
-      path: "./fonts/OKAYSERIF-BOLD.ttf",
+      path: "./OKAYSERIF-BOLD.ttf",
       weight: "500",
     },
   ],
